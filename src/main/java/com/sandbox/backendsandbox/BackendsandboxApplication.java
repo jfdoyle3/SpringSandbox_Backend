303a1adc0c03bf239595a94cf5f3a56e126beb63
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-02-15 03:04:57 -0500

    sandbox project started

---
+++ src/main/java/com/sandbox/backendsandbox/BackendsandboxApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.backendsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandbox/backendsandbox/BackendsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.backendsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
