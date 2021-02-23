e133c24120c5261a9a8f57d6e28b0d14041d6de8
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-02-23 18:16:50 -0500

    added controller and api

---
+++ src/main/java/com/sandbox/backendsandbox/controllers/MicroServiceController.java
@@ -0,0 +1,12 @@
+package com.sandbox.backendsandbox.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MicroServiceController {
+
+
+    public static void getNews(){
+
+    }
+}
