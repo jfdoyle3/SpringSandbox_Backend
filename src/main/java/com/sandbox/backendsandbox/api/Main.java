cc6b753b067155401093dc0e22fa0995e07d5bc9
Jim Doyle <indellibleink@gmail.com>
2021-03-07 01:50:48 -0500

    removed main class

--- src/main/java/com/sandbox/backendsandbox/api/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sandbox.backendsandbox.api;
-
-public class Main {
-    public static void main(String[] args) {
-
-//        String newsJson=NewsAPI.getNews(it);
-//        System.out.println(newsJson);
-    }
-}
